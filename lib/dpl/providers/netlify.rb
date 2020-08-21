4ec018666cde5d8d5328892cf4043b5ae8accc84
Jakub Koralewski <contact@jcubed.me>
2020-08-21 20:34:24 +0200

    debug pwd

--- lib/dpl/providers/netlify.rb
+++ lib/dpl/providers/netlify.rb
@@ -25,6 +25,7 @@ module Dpl
         output = shell "netlify deploy #{deploy_opts}", echo: false, capture:true
         info output
         if json
+          shell "pwd && ls -la"
           write_file "./NETLIFY_DEPLOY_JSON_ID_#{site}.json", output
         end
       end
